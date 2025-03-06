cc29742e93d137fbe0636a0d29efd96f3e703e7d
Dmitry Kovalev <trigger1610@gmail.com>
2025-03-06 17:37:34 +0400

    Fix tailwind init

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiled: './src/setupTests.ts',
-  },
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts"
+  }
 });
