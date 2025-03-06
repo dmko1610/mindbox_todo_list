ba8db02912cc094500e6689f45424768b3037eeb
Dmitry Kovalev <trigger1610@gmail.com>
2025-03-06 18:48:30 +0400

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -9,5 +9,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts"
-  }
+  },
+  base: "mindbox_todo_list"
 });
